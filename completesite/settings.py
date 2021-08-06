c5ec593ace799f0fd7e9a885d9be7b0bc1226690
ukaillatifmir <60869712+ukaillatifmir@users.noreply.github.com>
2021-08-06 17:19:41 +0530

    Used sqlite

--- completesite/settings.py
+++ completesite/settings.py
@@ -76,24 +76,24 @@ WSGI_APPLICATION = 'completesite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'blog',
-        'PORT':'3306',
-        'HOST':'localhost',
-        'USER':'root',
-        'PASSWORD':'',
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'blog',
+#        'PORT':'3306',
+#        'HOST':'localhost',
+#        'USER':'root',
+#        'PASSWORD':'',
         
 
+#    }
+#}
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
-    #DATABASES = {
-    #    'default': {
-    #        'ENGINE': 'django.db.backends.sqlite3',
-    #        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #    }
-    #}
 
 
 # Password validation
